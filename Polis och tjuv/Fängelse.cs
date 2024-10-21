ba649ae80a5d56c1500062be14076a1172f30c2b
Rabar Mohamed <rabar003@hotmail.com>
2024-10-20 19:52:12 -0700

    ska lägga till ett fängelse i min kod

---
+++ Polis och tjuv/Fängelse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polis_och_tjuv
+{
+    internal class Fängelse
+    {
+    }
+}
