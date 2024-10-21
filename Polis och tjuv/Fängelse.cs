a8225d8aeaf38ab4f2adec0580a42c316dd93dbc
Rabar Mohamed <rabar003@hotmail.com>
2024-10-20 20:03:22 -0700

    Klass för fängelse där vi hanterar gripna tjuvar

--- Polis och tjuv/Fängelse.cs
+++ Polis och tjuv/Fängelse.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Polis_och_tjuv
 {
-    internal class Fängelse
+    public class Fängelse
     {
+        public List<Tjuv> Fångar { get;private set; }
+
+        public Fängelse() 
+        { 
+            Fångar = new List<Tjuv>();
+        
+        }
+        
+        public void LäggTillFånge(Tjuv tjuv) 
+        { 
+            Fångar.Add(tjuv);
+            Console.WriteLine(" Tjuven har Plascerats i fängelse.");
+        
+        }
     }
 }
