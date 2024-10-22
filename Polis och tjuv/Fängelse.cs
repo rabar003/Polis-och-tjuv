d25bd7f7c598556c5fbc0d037887411ab17d1cc0
Rabar Mohamed <rabar003@hotmail.com>
2024-10-21 21:09:29 -0700

    kommentarer

--- Polis och tjuv/Fängelse.cs
+++ Polis och tjuv/Fängelse.cs
@@ -8,17 +8,20 @@ namespace Polis_och_tjuv
 {
     public class Fängelse
     {
+        // En lista som håller alla tjuvar som är fångar i fängelset
         public List<Tjuv> Fångar { get;private set; }
 
+        // Konstruktor för att skapa ett fängelse
         public Fängelse() 
         { 
-            Fångar = new List<Tjuv>();
-        
+            Fångar = new List<Tjuv>(); // Skapar en tom lista för fångarna
+
         }
-        
+
+        // Metod för att lägga till en tjuv till fängelset
         public void LäggTillFånge(Tjuv tjuv) 
         { 
-            Fångar.Add(tjuv);
+            Fångar.Add(tjuv); // Lägger tjuven till listan över fångar
             Console.WriteLine(" Tjuven har Plascerats i fängelse.");
         
         }
