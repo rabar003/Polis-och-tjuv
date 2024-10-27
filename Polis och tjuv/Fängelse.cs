2b5f375bec8a840de8a7de037da0bd5431be93cf
Rabar Mohamed <rabar003@hotmail.com>
2024-10-26 21:14:38 -0700

    uppdatera fängelse calssen så tjuvarna ska kunna röra på sig i fängelset

--- Polis och tjuv/Fängelse.cs
+++ Polis och tjuv/Fängelse.cs
@@ -8,6 +8,8 @@ namespace Polis_och_tjuv
 {
     public class Fängelse
     {
+        public int bredd = 6; // bredd på fängelset 
+        public int höjd = 10; // höjd på fängelset 
         // En lista som håller alla tjuvar som är fångar i fängelset
         public List<Tjuv> Fångar { get;private set; }
 
@@ -18,11 +20,25 @@ namespace Polis_och_tjuv
 
         }
 
+
         // Metod för att lägga till en tjuv till fängelset
         public void LäggTillFånge(Tjuv tjuv) 
         { 
+            tjuv.X = new Random().Next(bredd);
+            tjuv.Y = new Random().Next(höjd);
+
             Fångar.Add(tjuv); // Lägger tjuven till listan över fångar
             Console.WriteLine(" Tjuven har Plascerats i fängelse.");
+        
+        }
+
+        public void Uppdatera() 
+        {
+            foreach (var fånge in Fångar)
+            {
+                fånge.Move(höjd, bredd);
+            }
+        
         
         }
     }
